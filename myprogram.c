0ee41af558781dd5e9c0f4af56ecafd9afbcad6d
Fran <francesca.nardelli.24@um.edu.mt>
2024-10-17 22:01:13 +0200

    prompting for number of cats and reporting on number of pets

--- myprogram.c
+++ myprogram.c
@@ -1,5 +1,17 @@
-#include<stdio.h>
-int main() {
-    printf("Hello, world");
+
+#include <stdio.h>
+
+int main(void)
+
+{
+    int dogs;
+    int cats;
+    int pets;
+    printf("How many dogs do you have?\n");
+    scanf("%d", &dogs);
+    printf("How many cats do you have?\n");
+    scanf("%d", &cats);
+    pets = dogs + cats;
+    printf("So you have %d pet(s)!\n", pets);
     return 0;
 }
